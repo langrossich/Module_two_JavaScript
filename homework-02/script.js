10caea0504fa5deed576b1c68e66ed3fded68bfc
Aleksandr <langrossich@gmail.com>
2023-03-05 21:00:49 +0800

    homework-02

---
+++ homework-02/script.js
@@ -0,0 +1,12 @@
+let = oneNumber = prompt('Введите первое число');
+let = twoNumber = prompt('Введите второе число');
+
+oneNumber = parseInt(oneNumber);
+twoNumber = parseInt(twoNumber);
+
+console.log (oneNumber + twoNumber);
+console.log(oneNumber - twoNumber);
+console.log(oneNumber / twoNumber);
+console.log(oneNumber * twoNumber);
+console.log(oneNumber % twoNumber);
+
